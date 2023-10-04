b72eb28238c62a1bfc1ec6093cff49b5090f86d7
Bob Fornal <bob.fornal@leadingedje.com>
2023-10-04 09:49:41 -0400

    Combining Run and Code Editor Functionality

--- src/assets/developer-tools-for-non-developers/code-001/test.mangled.js
+++ src/assets/developer-tools-for-non-developers/code-001/test.mangled.js
@@ -1,1 +1,1 @@
-console.log("testing this");var myFunc1=()=>{encodeURI("\ud800")},myFunc2=()=>{a=l},myFunc3=()=>{null.f()};try{myFunc1()}catch(c){console.log(c)}try{myFunc2()}catch(c){console.log(c)}try{myFunc3()}catch(c){console.log(c)}(function*(){})().throw(new Error("example"));
+function dtfndInit002(){console.log("testing this");var o=()=>{encodeURI("\ud800")};var n=()=>{a=l};var t=()=>{null.f()};try{o()}catch(c){console.log(c)}try{n()}catch(c){console.log(c)}try{t()}catch(c){console.log(c)}(function*(){})()["throw"](new Error("example"))}
