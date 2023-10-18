9e7f9d39297d13d763a00415ff68960866e10767
Bob Fornal <bob.fornal@leadingedje.com>
2023-10-18 14:20:31 -0400

    Fixing Asset References

--- src/assets/developer-tools-for-non-developers/code-003/api-calls.js
+++ src/assets/developer-tools-for-non-developers/code-003/api-calls.js
@@ -1,11 +1,11 @@
 async function dtfndInit004() {
   const files = [
-    '/assets/developer-tools-for-non-developers/code-003/small.json',
-    '/assets/developer-tools-for-non-developers/code-003/medium.json',
-    '/assets/developer-tools-for-non-developers/code-003/small.json',
-    '/assets/developer-tools-for-non-developers/code-003/large.json',
-    '/assets/developer-tools-for-non-developers/code-003/small.json',
-    '/assets/developer-tools-for-non-developers/code-003/small.json',
+    '/#/assets/developer-tools-for-non-developers/code-003/small.json',
+    '/#/assets/developer-tools-for-non-developers/code-003/medium.json',
+    '/#/assets/developer-tools-for-non-developers/code-003/small.json',
+    '/#/assets/developer-tools-for-non-developers/code-003/large.json',
+    '/#/assets/developer-tools-for-non-developers/code-003/small.json',
+    '/#/assets/developer-tools-for-non-developers/code-003/small.json',
   ]
   try {
     for (let i = 0, len = files.length; i < len; i++) {
