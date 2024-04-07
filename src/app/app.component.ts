5f1c7136fcf16886ccf15502aaf59566d487d3d1
Bob Fornal <bob.fornal@leadingedje.com>
2024-04-07 11:01:25 -0400

    Vercel Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { APP_INITIALIZER, Component, isDevMode } from '@angular/core';
 import { CodeService } from './core/services/code.service';
 import { RouterOutlet } from '@angular/router';
+
+import { inject } from "@vercel/analytics"
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [RouterOutlet]
+    imports: [RouterOutlet],
+    providers: [{
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        inject({ mode: isDevMode() ? 'development' : 'production' });
+      }
+    }]
 })
 export class AppComponent {
   structure: any = {
