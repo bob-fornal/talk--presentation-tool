a44521dd4f04e746751fd22b8897257553afdd12
Bob Fornal <bob.fornal@leadingedje.com>
2024-07-02 20:34:06 -0400

    Editing Image Only and Panel Double

--- src/app/slides/panel-double/panel-double.component.ts
+++ src/app/slides/panel-double/panel-double.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+import { EditNotesDialogComponent } from 'src/app/shared/edit-notes-dialog/edit-notes-dialog.component';
 
 @Component({
     selector: 'panel-double',
@@ -10,34 +14,70 @@ import { MatButtonModule } from '@angular/material/button';
       './panel-double.component.scss'
     ],
     standalone: true,
-    imports: [MatButtonModule]
+    imports: [
+      MatButtonModule,
+      MatDialogModule,
+    ],
 })
 export class PanelDoubleComponent {
   @Input() title: string = '';
   @Input() textLeft: string = '';
   @Input() textRight: string = '';
+  @Input() notes: string = '';
+
   @Input() editing: boolean = false;
+
   @Input() fontsize: string | undefined = undefined;
 
   @Output() save: EventEmitter<any> = new EventEmitter();
 
   constructor(
+    private dialog: MatDialog,
     private route: ActivatedRoute,
     private router: Router
   ) {}
-  
+
   saveEvent = (): void => {
-    const response: any = { type: 'save', items: [] };
+    const response: any = { ACTION: 'save', ITEMS: [] };
     const elements: any = document.querySelectorAll('[data-editing]');
     for (let i = 0, len = elements.length; i < len; i++) {
       const item = elements[i];
       const key = item.dataset.editing;
-      response.items.push(key);
-      response[key] = item.innerText;
+      response.ITEMS.push(key);
+      response[key] = item.value;
     }
+
+    response.ITEMS.push('notes');
+    response.notes = this.notes;
+    response.slideKey = this.route.snapshot.paramMap.get('slideKey');
+    
     this.save.emit(response);
 
     const path: string = this.route.snapshot.paramMap.get('folder') || '';
     this.router.navigate(['edit', path]);
+  };
+
+  cancelEvent = (): void => {
+    const response: any = { ACTION: 'cancel' };
+    this.save.emit(response);
+
+    const path: string = this.route.snapshot.paramMap.get('folder') || '';
+    this.router.navigate(['edit', path]);
+  };
+
+  editNotes = (): void => {
+    const notes = this.notes.toString();
+    const dialogRef = this.dialog.open(EditNotesDialogComponent, {
+      data: { notes },
+    });
+
+    dialogRef.afterClosed().subscribe(this.handleEditNotesClosed.bind(this));
+  };
+
+  handleEditNotesClosed = (result: any): void => {
+    if (result === undefined || result.type === 'cancel') return;
+
+    this.notes = result.data.notes;
+    console.log(result, this.notes);
   };
 }
