5b376440ec9640da9f41c4255f973ec204275b2f
bob-fornal <bob-fornal@users.noreply.github.com>
2024-07-06 16:18:42 +0000

    deploy: fd7b683af1a81ea987d10425be3c4b55c360d2b1

---
+++ assets/talks/developer-tools-for-non-developers/code-003/api-calls.js
@@ -0,0 +1,24 @@
+async function dtfndInit004() {
+  const files = [
+    './assets/talks/developer-tools-for-non-developers/code-003/small.json',
+    './assets/talks/developer-tools-for-non-developers/code-003/medium.json',
+    './assets/talks/developer-tools-for-non-developers/code-003/small.json',
+    './assets/talks/developer-tools-for-non-developers/code-003/large.json',
+    './assets/talks/developer-tools-for-non-developers/code-003/small.json',
+    './assets/talks/developer-tools-for-non-developers/code-003/small.json',
+  ]
+  try {
+    for (let i = 0, len = files.length; i < len; i++) {
+      const response = await fetch(files[i], {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
