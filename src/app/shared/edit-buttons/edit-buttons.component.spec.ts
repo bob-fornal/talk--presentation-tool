966bc379dc67a88b533e9b1cc5aab29989f01a44
Bob Fornal <bob.fornal@leadingedje.com>
2024-07-13 18:42:44 -0400

    Edit Buttons: Unit Test Coverage

--- src/app/shared/edit-buttons/edit-buttons.component.spec.ts
+++ src/app/shared/edit-buttons/edit-buttons.component.spec.ts
@@ -24,4 +24,13 @@ describe('EditButtonsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('expects "triggerToggleView" to toggle and emit state', () => {
+    component.toggleView = false;
+    spyOn(component.toggle, 'emit').and.stub();
+
+    component.triggerToggleView();
+    expect(component.toggleView).toEqual(true);
+    expect(component.toggle.emit).toHaveBeenCalledWith(true);
+  });
 });
