6c217a45e29bbc6f8486890d03db165d8b832327
Bob Fornal <bob.fornal@leadingedje.com>
2024-11-22 13:17:49 -0500

    Working thru Failures (2)

---
+++ src/app/_spec/components/mock-content-section.component.spec.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'content-section',
+  template: '<h1>content-section</h1>',
+})
+export class MockContentSection {
+  @Input() index: number = 0;
+  @Input() last?: boolean = false;
+  @Input() circleOnly?: boolean = false;
+  @Input() biography?: boolean = false;
+  @Input() dual?: boolean = false;
+}
--- src/app/pages/events/events.component.spec.ts
+++ src/app/pages/events/events.component.spec.ts
@@ -2,13 +2,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventsComponent } from './events.component';
 
+import { MockContentSection } from '../../_spec/components/mock-content-section.component.spec';
+
+import { EventsService } from '../../core/services/events.service';
+import { MockEventsService } from '../../_spec/services/mock-event.service.spec';
+
 describe('EventsComponent', () => {
   let component: EventsComponent;
   let fixture: ComponentFixture<EventsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EventsComponent]
+      declarations: [
+        EventsComponent,
+
+        MockContentSection,
+      ],
+      providers: [
+        { provide: EventsService, useValue: MockEventsService },
+      ]
     })
     .compileComponents();
 
