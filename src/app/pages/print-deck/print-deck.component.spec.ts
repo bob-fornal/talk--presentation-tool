451a9b5b9faba2525e24ff3d78f6cb502a9e2134
Bob Fornal <bob.fornal@leadingedje.com>
2026-04-30 17:45:45 -0400

    Fixing Tests

--- src/app/pages/print-deck/print-deck.component.spec.ts
+++ src/app/pages/print-deck/print-deck.component.spec.ts
@@ -2,13 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PrintDeckComponent } from './print-deck.component';
 
+import { ActivatedRoute } from '@angular/router';
+import { MockActivatedRoute } from '../../_spec/mock-activated-route.spec';
+
+import { CodeService } from '../../core/services/code.service';
+import { MockCodeService } from '../../_spec/services/mock-code.service.spec';
+
 describe('PrintDeckComponent', () => {
   let component: PrintDeckComponent;
   let fixture: ComponentFixture<PrintDeckComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PrintDeckComponent]
+      declarations: [PrintDeckComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: MockActivatedRoute },
+        { provide: CodeService, useValue: MockCodeService },
+      ],
     })
     .compileComponents();
 
